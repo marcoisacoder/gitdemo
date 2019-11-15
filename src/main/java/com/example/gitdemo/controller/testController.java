0b8eb3dda62f29ec9f7953b35eaa63e41b19ef3c
SongLingyu <1084518530@qq.com>
2019-11-15 12:09:04 +0800

    modify testController in branch develop..

---
+++ src/main/java/com/example/gitdemo/controller/testController.java
@@ -0,0 +1,14 @@
+package com.guwukeji.varysmcn;
+
+/**
+ * @program: varys-parent
+ * @description: test class
+ * @author: songlingyu
+ * @create: 2019-11-15 11:48
+ */
+public class testClass {
+    public static void main(String[] args) {
+        System.out.println("this is test class.");
+	System.out.println("add modify for branch develop..");
+    }
+}
