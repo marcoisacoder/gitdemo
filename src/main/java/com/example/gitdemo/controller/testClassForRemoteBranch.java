a1c214f8ae8d117b4e3516b51ab80f726725cd50
SongLingyu <1084518530@qq.com>
2019-11-15 17:11:06 +0800

    store modify for remote Branch

---
+++ src/main/java/com/example/gitdemo/controller/testClassForRemoteBranch.java
@@ -0,0 +1,19 @@
+package com.guwukeji.varysmcn;
+
+/**
+ * @program: varys-parent
+ * @description: test class
+ * @author: songlingyu
+ * @create: 2019-11-15 11:48
+ */
+public class testClass {
+    public static void main(String[] args) {
+        System.out.println("this is test class.");
+
+	System.out.println("this is first modify for branch bug002..");
+
+	System.out.println("this is second modify for branch bug002.."):
+
+	//this is test modify for remote branch
+    }
+}
