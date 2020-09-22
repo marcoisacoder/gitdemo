9dbe182033b1d69895fad1c1939bfd770e700686
songlingyu <1084518530@qq.com>
2020-09-22 16:28:12 +0800

    add test0922

--- src/main/java/com/example/gitdemo/GitdemoApplication.java
+++ src/main/java/com/example/gitdemo/GitdemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitdemoApplication {
 
 	public static void main(String[] args) {
+		//add some test words
 		SpringApplication.run(GitdemoApplication.class, args);
 	}
 
